d552dd19079925bc3074fae5868beec2ac9589c5
Shone <ShoneKey@163.com>
2017-05-22 17:29:39 +0800

    'chapter_3d'

--- hello.py
+++ hello.py
@@ -1,14 +1,20 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 from flask_script import Manager
+
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route("/user/<name>")
