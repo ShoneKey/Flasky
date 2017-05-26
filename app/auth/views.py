9d69955ddfffe7cbbf4d8b339dd80fed56ce246f
Shone <ShoneKey@163.com>
2017-05-26 17:17:34 +0800

    'chapter_8d'

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,8 @@ from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user
 
 from . import auth
-from ..models import User
-from .forms import LoginForm
+from ..models import User, db
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -24,3 +24,16 @@ def logout():
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
